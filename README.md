d5635ea6ab23775a5655486f5fc502a2166762a6
Zach Churchill <zachurchill@protonmail.com>
2023-12-01 17:03:21 -0500

    move python scripts to module and add runner script

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # aoc2023
+
+## Python
+
+To run the solutions for individual days utilize the `run_python.py` script.
+It will prompt you with the available days to choose from and run the selected script's `solve` function.
---
+++ run_python.py
@@ -0,0 +1,17 @@
+import argparse
+import importlib
+import os
+import re
+
+AOCPY_MOD = "aocpy"
+AOCPY_DIR = os.path.join(os.path.dirname(__file__), AOCPY_MOD)
+days_solved = [
+    m.group() for f in os.listdir(AOCPY_DIR) if (m := re.match(r"\d", f))
+]
+
+parser = argparse.ArgumentParser("Runs Python AoC solutions")
+parser.add_argument("day", choices=days_solved)
+args = parser.parse_args()
+
+selected_day = importlib.import_module(f"{AOCPY_MOD}.{args.day}")
+selected_day.solve()
