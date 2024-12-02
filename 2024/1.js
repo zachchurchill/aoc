59d89482fa84f9f89530227e3688454ec84deebd
Zach Churchill <zachurchill@protonmail.com>
2024-12-01 21:51:10 -0500

    day 1 part 2

--- 2024/1.js
+++ 2024/1.js
@@ -23,3 +23,13 @@ const totalDistance = leftLocationIds
 console.log(`Part 1 answer: ${totalDistance}`);
 
 // Construct a map with the values as keys & occurences summed as values then compare/iterate
+const rightIds = new Map();
+for (const el of rightLocationIds) {
+  rightIds.set(el, (rightIds.get(el) ?? 0) + 1);
+}
+
+let total = 0;
+for (const el of leftLocationIds) {
+  total += el * (rightIds.get(el) ?? 0);
+}
+console.log(`Part 2 answer: ${total}`);
